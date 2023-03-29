a887841a2e855853e04d052893d6c8c377b5e2bb
Jcodes25 <122479619+Jcodes25@users.noreply.github.com>
2023-03-29 14:34:40 -0400

    added enemy and enemy movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize pygame
 pygame.init()
@@ -17,10 +18,20 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+enemyImg = pygame.image.load('Enemy.png')
+enemyX = random.randint(0,800)
+enemyY = random.randint(50,150)
+enemyX_change = 0.3
+enemyY_change = 40
+
 
 # Creates player function
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # Game Loop
@@ -34,7 +45,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # if keystroke is pressed check whether its right or left
+        # if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerX_change = -0.3
@@ -51,5 +62,18 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
+    # Enemy Movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
+
+
