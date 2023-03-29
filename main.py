9cb1ccd76cebe93c8d2bc504253580610c6a4e67
Jcodes25 <122479619+Jcodes25@users.noreply.github.com>
2023-03-29 01:48:29 -0400

    added player movement and boundaries

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-
-#Title and Icon
+# Title and Icon
 pygame.display.set_caption('Space Invader')
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
@@ -15,16 +14,42 @@ pygame.display.set_icon(icon)
 # Player
 playerImg = pygame.image.load('player.png')
 playerX = 370
-PlayerY = 480
+playerY = 480
+playerX_change = 0
 
 
-#RGB Value
-screen.fill((0, 0, 0))
-pygame.display.update()
+# Creates player function
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
-#Game Loop
+
+# Game Loop
 running = True
 while running:
+
+    # RGB Value
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    player(playerX, playerY)
+    pygame.display.update()
